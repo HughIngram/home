f74332b73577735f03f1539a45595080193bd91e
Hugh Ingram <hughi94@gmail.com>
2015-01-20 14:54:23 +0000

    reliability of backgrounds script improved

--- backgrounds.sh
+++ backgrounds.sh
@@ -1,4 +1,3 @@
-#to do: keep original filename and do not delete old images
 #!/bin/bash
 
 #script to download the APOD from NASA and set it as the desktop background
@@ -7,34 +6,32 @@ cd $HOME/Pictures
 mkdir -p apod 
 cd apod
 #pwd
-rm  -fv --one-file-system *.html *.jpg *.jpeg *.png
+rm  -fv --one-file-system astropix.html
 echo
 
 echo downloading web page http://apod.nasa.gov/apod/astropix.html
-echo
-wget -v --no-use-server-timestamps http://apod.nasa.gov/apod/astropix.html
+echo ===========================
+wget -v http://apod.nasa.gov/apod/astropix.html
+echo ===========================
 
-yearmonth=$(date --date="yesterday" +"%y%m")
-wholeline=$(grep -m 1 $yearmonth astropix.html)
+wholeline=$(grep -m 1 '<a href="image' astropix.html)
 
-echo
 #echo wholeline = $wholeline
 wholeline=${wholeline%??}
 #echo wholeline = $wholeline
 wholeline=${wholeline#?????????}
 #echo wholeline= $wholeline
-#echo
+echo
 echo Downloading image at http://apod.nasa.gov/apod/$wholeline
 echo
-wget -N "http://apod.nasa.gov/apod"/$wholeline 
+wget -Nv "http://apod.nasa.gov/apod"/$wholeline 
+echo ===========================
+filename=$(basename $wholeline)
+echo downloaded image:
+ls -lh | grep $filename
 echo
-echo ============= mv -vf =============
-mv -vf *.jpg background.jpg
-mv -vf *.png background.png
-echo ==================================
-
-gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/apod/background.png
-gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/apod/background.jpg
+echo Script Complete!
+echo ===========================
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/apod/$filename
 gsettings set org.gnome.desktop.background picture-options zoom
-echo
-echo background changed succesfully
+
