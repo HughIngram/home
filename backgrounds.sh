b2acfdbcbc98388f58d29953b24d701870471d07
HughIngram <HughIngram@users.noreply.github.com>
2017-05-18 00:47:30 +0100

    fixed lint errors

--- backgrounds.sh
+++ backgrounds.sh
@@ -39,23 +39,23 @@ fi
 wholeline=${wholeline%??}
 img_address=${wholeline#??????????}
 
-wget -Nv https://apod.nasa.gov/apod/$img_address -P "$path_to_images"
-filename="$(basename "$path_to_images/$img_address")"
+wget -Nv "https://apod.nasa.gov/apod/$img_address" -P "$path_to_images"
+path_to_image_file="$path_to_images/""$(basename "$path_to_images/$img_address")"
 
-height="$(identify -format %h "$path_to_images/$filename")"
-width="$(identify -format %w "$path_to_images/$filename")"
-filesize="$(wc -c < "$path_to_images/$filename")"
+height="$(identify -format %h "$path_to_image_file")"
+width="$(identify -format %w "$path_to_image_file")"
+filesize="$(wc -c < "$path_to_image_file")"
 
 if (( filesize < MIN_IMG_SIZE
-	|| width <= MIN_IMG_WIDTH || height <= MIN_HEIGHT ))
+	|| width <= MIN_IMG_WIDTH || height <= MIN_IMG_HEIGHT ))
 	then
 		echo "Image too small."
 		exit 1
 fi
 
 echo "==========================="
-echo "file://$HOME/Pictures/apod/$path_to_images/$filename"
-gsettings set org.gnome.desktop.background picture-uri file://$path_to_images/$filename
+echo "file://$HOME/Pictures/apod/$path_to_image_file"
+gsettings set org.gnome.desktop.background picture-uri file://"$path_to_image_file"
 gsettings set org.gnome.desktop.background picture-options zoom
 
 rm  -fv "$path_to_images/astropix.html"
