75d898af69077aee351db15c307b829f3a3b966b
Hugh Ingram <hin@medicuja.com>
2019-11-03 16:54:17 +0100

    simplifies installation

--- set_up_home.sh
+++ set_up_home.sh
@@ -20,52 +20,32 @@ if [ ! $? -eq 0 ]; then
 	exit 2
 fi
 
-cd $HOME
 
 #install various programs
 
 apt install htop filezilla terminator vim gcc make ghc
 
-#install git then download and install 'home' repo
-apt-get install git
-if [ $? -eq 0 ]; then
-	log+="git installed succesfully...\n";
-	
-	home_repo="home"
-	if [ ! -e "$home_repo" ]; then
-		log+="home repo not found locally. Cloning home repo...\n"; 
-		git clone https://github.com/HughIngram/home.git;
-	else
-		log+="home repo already exists. Using existing version\n";
-	fi
-	
-	cd $HOME
-	if [  -e "$home_repo" ]; then
-		cd "$HOME""/""$home_repo";
-		cp -v .bash_profile ..;
-		cp -v .bashrc ..;
-		cp -v .vimrc ..;
-		cp -v .xmodmap-esc ..;
-		cp -v backgrounds.sh ..;
-		cp -v random_background_selector.sh ..;
-		if [ $? -eq 0 ]; then
-			log+="files in home repo copied succesfuly\n";
-		else
-			log+="files in home repo not copied\n";
-		fi
-		
-		#the below is only executed if the computer is a ThinkPad laptop
-		if [[ -n "$(sudo dmidecode | grep ThinkPad)" ]]; then
-			log+="lubuntu-rc.xml copied\n";
-			cp -v lubuntu-rc.xml $HOME/.config/openbox/lubuntu-rc.xml;
-		else
-			log+="lubuntu-rc.xml not copied\n";
-		fi
-	fi
-	
-else 
-	log+="git not installed succesfully\n";
-	exit 1;
+#install install this repo
+
+cd $HOME
+home_repo="home"
+cd $HOME
+if [  -e "$home_repo" ]; then
+    cd "$HOME""/""$home_repo";
+    cp -v .bash_profile ..;
+    cp -v .bashrc ..;
+    cp -v .vimrc ..;
+    cp -v .xmodmap-esc ..;
+    cp -v backgrounds.sh ..;
+    cp -v random_background_selector.sh ..;
+    
+    #the below is only executed if the computer is a ThinkPad laptop
+    if [[ -n "$(sudo dmidecode | grep ThinkPad)" ]]; then
+        log+="lubuntu-rc.xml copied\n";
+        cp -v lubuntu-rc.xml $HOME/.config/openbox/lubuntu-rc.xml;
+    else
+        log+="lubuntu-rc.xml not copied\n";
+    fi
 fi
-echo -e "$log"
+	
 exit
